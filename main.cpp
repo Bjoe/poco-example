4dc08cd9da6fd6f49a75c4dd55c50aa38ecdb33c
Joerg-Christian Boehme <joerg@chaosdorf.de>
2021-04-20 18:43:40 +0200

    Example with hunter

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+
 #include <iostream>
 
-int main()
+#include <Poco/Crypto/X509Certificate.h>
+
+int main(int argc, char** argv)
 {
-  std::cout << "Hello Poco\n";
+  std::cout << "Hello hunter Poco\n";
+
+  Poco::Crypto::X509Certificate cert;
+
   return 0;
 }
